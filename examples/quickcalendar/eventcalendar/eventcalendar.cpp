b6cad062ab6be9816743bdf7f0931308edea2f18
Mitch Curtis <mitch.curtis@qt.io>
2021-07-16 12:01:04 +0200

    Fix build
    The event API changed, so update our overrides.
    
    Also fix a warning about high DPI.
    
    Task-number: QTEXT-8
    Change-Id: Ieb0fbb13dd9d831373fcc6da0c2c42b594216b82
    Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>

--- examples/quickcalendar/eventcalendar/eventcalendar.cpp
+++ examples/quickcalendar/eventcalendar/eventcalendar.cpp
@@ -59,8 +59,6 @@ int main(int argc, char *argv[])
     if (qEnvironmentVariableIsEmpty("QTGLESSTREAM_DISPLAY")) {
         qputenv("QT_QPA_EGLFS_PHYSICAL_WIDTH", QByteArray("213"));
         qputenv("QT_QPA_EGLFS_PHYSICAL_HEIGHT", QByteArray("120"));
-
-        QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     }
     QGuiApplication app(argc, argv);
 
--- src/imports/calendar/qquickmonthgrid.cpp
+++ src/imports/calendar/qquickmonthgrid.cpp
@@ -108,9 +108,9 @@ public:
     void updatePress(const QPointF &pos);
     void clearPress(bool clicked);
 
-    void handlePress(const QPointF &point) override;
-    void handleMove(const QPointF &point) override;
-    void handleRelease(const QPointF &point) override;
+    void handlePress(const QPointF &point, ulong timestamp) override;
+    void handleMove(const QPointF &point, ulong timestamp) override;
+    void handleRelease(const QPointF &point, ulong timestamp) override;
     void handleUngrab() override;
 
     static void setContextProperty(QQuickItem *item, const QString &name, const QVariant &value);
@@ -181,24 +181,24 @@ void QQuickMonthGridPrivate::clearPress(bool clicked)
     pressedItem = nullptr;
 }
 
-void QQuickMonthGridPrivate::handlePress(const QPointF &point)
+void QQuickMonthGridPrivate::handlePress(const QPointF &point, ulong timestamp)
 {
     Q_Q(QQuickMonthGrid);
-    QQuickControlPrivate::handlePress(point);
+    QQuickControlPrivate::handlePress(point, timestamp);
     updatePress(point);
     if (pressedDate.isValid())
         pressTimer = q->startTimer(qGuiApp->styleHints()->mousePressAndHoldInterval());
 }
 
-void QQuickMonthGridPrivate::handleMove(const QPointF &point)
+void QQuickMonthGridPrivate::handleMove(const QPointF &point, ulong timestamp)
 {
-    QQuickControlPrivate::handleMove(point);
+    QQuickControlPrivate::handleMove(point, timestamp);
     updatePress(point);
 }
 
-void QQuickMonthGridPrivate::handleRelease(const QPointF &point)
+void QQuickMonthGridPrivate::handleRelease(const QPointF &point, ulong timestamp)
 {
-    QQuickControlPrivate::handleRelease(point);
+    QQuickControlPrivate::handleRelease(point, timestamp);
     clearPress(true);
 }
 
